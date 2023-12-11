6b21c407cb972f808d014a57124b79c5ef9a6fb5
Marcelinne <mmontalvo@sodigsa.com>
2023-12-11 17:22:35 -0500

    feat (forms.py) Validate all text field at once to allow the calculation

--- base_layout_David/version_2/src/forms.py
+++ base_layout_David/version_2/src/forms.py
@@ -1,3 +1,4 @@
+#Import libraries for the design 
 from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.core.window import Window
@@ -8,53 +9,47 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.app import App
 from kivymd.app import MDApp
+#Import packages for the logic
+from calculator import Rankine_P_Close
 
 
 class MainApp(MDApp):
     
     def build(self):
         Window.size = (1150, 600)
-        #dark theme
-
+        #Theme
         self.theme_cls.theme_style = "Light"
-        #light theme
+        #Colors
         self.theme_cls.primary_palette = "Orange"
         self.theme_cls.accent_palette = "Red"
-
         return Builder.load_file('../design/forms.kv')
-    def login(self):
-        self.root.ids.welcome_label.text = "Hello " + self.root.ids.user.text
-    def clear(self):
-        self.root.ids.welcome_label.text = "Welcome"
-        self.root.ids.user.text = ""
-        self.root.ids.password.text = ""
-
-    #Messages control for the inputs
-    def set_error_message(self, instance_textfield):
-        self.screen.ids.p1.error = True
     
-    def validate_textfield(self):
-        text_field = self.root.ids.p1
-        input_text = text_field.text.strip()
 
+    def validate_textfield(self, text_field, min_limit, max_limit):
+        input_text = text_field.text.strip()
         try:
             input_value = int(input_text)  # Intentar convertir el texto a un entero
-            if 3 <= input_value <= 30:
+            if min_limit <= input_value <= max_limit:
                 text_field.helper_text = ""
                 text_field.error = False
                 text_field.text_color = 1, 1, 1, 1  # Color de texto normal
+                return True
             else:
-                text_field.helper_text = "Valor fuera de rango (3-30)"
+                text_field.helper_text = f"Valor fuera de rango ({min_limit}-{max_limit})"
                 text_field.error = True
                 text_field.text_color = 1, 0, 0, 1  # Rojo
+                return False
         except ValueError:
             text_field.helper_text = "Ingrese un número válido"
             text_field.error = True
             text_field.text_color = 1, 0, 0, 1  # Rojo
 
-#Colors: Red, Pink, Purple, DeepPurple,
-# Indigo, Blue, LightBlue, Cyan, Teal,
-# Green, LightGreen, Lime, Yellow, Amber,
-# Orange, DeepOrange, Brown, Gray, BlueGray
+    #Function to validate every textfield that returns true if all the textfields are correct
+    def validate_all_textfields(self):
+        #Validate the textfields
+        if self.validate_textfield(self.root.ids.p1, 3, 30) and self.validate_textfield(self.root.ids.p2, 2000, 36000) and self.validate_textfield(self.root.ids.p3, 1000, 22000) and self.validate_textfield(self.root.ids.T6, 200, 800) and self.validate_textfield(self.root.ids.mp, 0.2, 1) and self.validate_textfield(self.root.ids.nb, 0.2, 1) and self.validate_textfield(self.root.ids.nt, 0.2, 1):
+            return True
+        else:
+            return False
 
 MainApp().run()
